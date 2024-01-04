259f40f53350e6fb42b3a3e167ad3107144ffb02
AndresBerdu <andres700crac@gmail.com>
2024-01-03 22:29:54 -0500

    neuvos cambios

--- uso-use-state/src/App.js
+++ uso-use-state/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import "./App.css";
+import React, { Fragment } from 'react';
+import { Testimonial } from "./components/Testimonial";
+import Photo1 from "./images/Foto1.png";
+import Photo2 from "./images/Foto2.jpg";
+import Photo3 from "./images/Foto3.jpg"; 
 
 function App() {
   return (
-    <div>App</div>
+    <Fragment>
+      <Testimonial 
+        img={Photo1} 
+        name="Juan Perez"
+        stars="⭐⭐⭐"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. At sapiente obcaecati debitis veniam quidem nemo beatae inventore. Animi corporis ducimus blanditiis quaerat eos a excepturi, cupiditate exercitationem, iusto architecto corrupti!"
+      />
+      <Testimonial
+        img={Photo2} 
+        name="Maria Suarez"
+        stars="⭐⭐⭐⭐"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. At sapiente obcaecati debitis veniam quidem nemo beatae inventore. Animi corporis ducimus blanditiis quaerat eos a excepturi, cupiditate exercitationem, iusto architecto corrupti!"
+      />
+      <Testimonial
+        img={Photo3} 
+        name="Camila Perez"
+        stars="⭐⭐⭐⭐⭐"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. At sapiente obcaecati debitis veniam quidem nemo beatae inventore. Animi corporis ducimus blanditiis quaerat eos a excepturi, cupiditate exercitationem, iusto architecto corrupti!"  
+      />
+    </Fragment>  
   )
 }
 
-export { App }
+export { App }; 
---
+++ uso-use-state/src/components/Testimonial.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Testimonial.css";
+
+function Testimonial({img, name, stats, text}) {
+  return (
+    <div className="Testimonial__container">
+
+        <div>
+          <img className="Testimonial__photo" src={img} alt="photo profile" />
+          <h2 className="Testimonial__name">{name}</h2>
+        </div>
+        
+        <div className="Testimonial__content">
+            <p className="Testimonial__starts">{stats}</p>
+            <p className="Testimonial__text">{text}</p>
+        </div>
+    </div>
+  )
+}
+
+export { Testimonial };
